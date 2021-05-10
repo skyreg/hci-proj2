af7cd1373218bcf20080bbcb4ee1600ea4f1b6ec
Jaylen Bailey <jaylenb4@gmail.com>
2021-05-10 18:33:37 -0400

    working boundaries and white color change indicator and red circles

--- Assets/Scripts/WebsocketZedVisualizer.cs
+++ Assets/Scripts/WebsocketZedVisualizer.cs
@@ -169,28 +169,137 @@ public class WebsocketZedVisualizer : MonoBehaviour
     //Debug.Log(avatar_transforms[id]["PELVIS"].localRotation);
     //avatar_transforms[id]["PELVIS"].localRotation = Quaternion.identity;
 
-    //add collider here
-    sphere_collider = track.gameObject.AddComponent<SphereCollider>();
-    // sphere_collider.transform = track.transform;
-    //sphere_collider.center = Vector3.zero; // centers at the origin - how can we center this?
-    sphere_collider.radius = 1.8288f; // i think this is 6 feet in Unity coordinates
-    // Debug.Log(track);
-    float a=0;
-    float x=0;
-    float y=0;
-    float z=0;
+    float [] x_array = new float[20];
+    float [] y_array = new float[20];
+    float [] z_array = new float[20];
+    int count_joints = 0;  
+    // TODO: find medians and kick out the outlier points
+
     foreach (KeyValuePair<string, SimpleJSON.JSONNode> joint in joints) {
-      x+=joint.Value[0];
-      y+=joint.Value[1];
-      z+=joint.Value[2];
-      a+=1;
+      if (joint.Value[9] == 0) { continue; }
+      if (joint.Value[2] >= 0 && joint.Value[2] <= 0.1) {
+        continue;
+      }
+      if (UseAvatar) {
+        PoseAvatarJoint(avatar, id, joint.Key, joint.Value);
+      }
+      if (UseSkeleton) {
+        PositionAndColorJoint(id, joint.Key, FindOrCreateJoint(skeleton, joint.Key), joint.Value);
+      }
+      
+      x_array[count_joints] = joint.Value[0];
+      y_array[count_joints] = joint.Value[1];
+      z_array[count_joints] = joint.Value[2];
+      count_joints += 1;
+
     }
-    x = x/a;
-    y = y/a;
-    z = z/a;
-    sphere_collider.center = CameraToUnity(x, y, z);
-    //sphere_collider.GetComponent<Renderer>().material.color = Color.blue;
-    //Debug.Log(x + " " + y + " " + z);
+    
+    if(skeleton.Find("Boundary") == null)
+    {
+      //Debug.Log("track-" + id);
+      float avgx = 0;
+      float avgz = 0;
+      int count = 0;
+      foreach (KeyValuePair<string, SimpleJSON.JSONNode> joint in joints) {
+        if(joint.Key == "LAnkle" || joint.Key == "RAnkle"){
+          avgx += joint.Value[0];
+          avgz += joint.Value[2];
+          count+=1;
+          //Debug.Log(joint.Key + " " + joint.Value[0] + " " + joint.Value[2]);
+        }
+      }
+      if(1 == 1){
+        float avg1 = avgx/count;
+        float avg2 = avgz/count;
+        //Debug.Log("avg1: " + avg1 + " avg2: " + avg2);
+        Vector3 pos = CameraToUnity(UnitConversion*avg1, (float)1.5, UnitConversion*avg2);
+        //Debug.Log("pos: " + pos);
+        GameObject boundary = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        boundary.name = "Boundary";
+        boundary.transform.position = pos;
+        boundary.transform.localScale = new Vector3(1.8288f, 0.000000001f, 1.8288f);
+        boundary.transform.parent = skeleton;
+        boundary.gameObject.GetComponent<Renderer>().material.color = Color.green;
+      }
+    }
+
+    float median_x = 0;
+    float median_y = 0;
+    float median_z = 0;
+    int index = 0;
+    if (count_joints % 2 == 0)
+    {
+      index = count_joints / 2;
+    }
+
+    else
+    {
+      index = (count_joints + 1) /2; 
+    }
+
+    median_x = x_array[index];
+    median_y = y_array[index];
+    median_z = z_array[index];
+
+    //Vector3 median_pos = CameraToUnity(UnitConversion*median_x, UnitConversion*median_y, UnitConversion*median_z);
+    Vector3 median_pos = skeleton.Find("Boundary").transform.position;
+    bool has_overlap = false;
+    foreach (Transform other_skeleton in Skeletons) {
+        if (other_skeleton != skeleton) {
+            float [] other_x_array = new float[20];
+            float [] other_y_array = new float[20];
+            float [] other_z_array = new float[20];
+            int other_count_joints = 0;  
+            Transform next_skeleton = other_skeleton.Find("Skeleton");
+            foreach (KeyValuePair<string, string> x in coco_to_rocketbox) {
+                //Debug.Log("find " + x.Key);
+                Transform found_joint = next_skeleton.Find(x.Key);
+                if (found_joint){
+                  //Debug.Log("found " + x.Key + " at " + found_joint.position);
+                  Vector3 found_position = found_joint.position;
+                  if (found_position.z >= 0 && found_position.z <= 0.1) {
+                    continue; 
+                  }
+                  other_x_array[other_count_joints] = found_position.x;
+                  other_y_array[other_count_joints] = found_position.y;
+                  other_z_array[other_count_joints] = found_position.z;
+                  other_count_joints += 1;
+                }  
+            }
+
+
+            float other_median_x = 0;
+            float other_median_y = 0;
+            float other_median_z = 0;
+            int other_index = 0;
+            if (other_count_joints % 2 == 0)
+            {
+              other_index = other_count_joints / 2;
+            }
+
+
+            else
+            {
+              other_index = (other_count_joints + 1) /2; 
+            }
+
+            other_median_x = other_x_array[other_index];
+            other_median_y = other_y_array[other_index];
+            other_median_z = other_z_array[other_index];
+            //Vector3 check_median_pos = CameraToUnity(UnitConversion*other_median_x, UnitConversion*other_median_y, UnitConversion*other_median_z);
+            Vector3 check_median_pos = next_skeleton.Find("Boundary").position;
+            float distance = Vector3.Distance(median_pos, check_median_pos);
+            Debug.Log("distance: " + distance);
+            //Debug.Log(median_pos + " " + other_skeleton.position + " " + distance);
+            //1.8288
+            skeleton.Find("Boundary").gameObject.GetComponent<Renderer>().material.color = Color.green;
+            if (distance <= 1.8288 && distance != 0) {
+                has_overlap = true;
+                skeleton.Find("Boundary").gameObject.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            }
+        }
+        }
 
     foreach (KeyValuePair<string, SimpleJSON.JSONNode> joint in joints) {
       if (joint.Value[9] == 0) { continue; }
@@ -202,20 +311,26 @@ public class WebsocketZedVisualizer : MonoBehaviour
       }
     }
 
+    
+
     // Compute joint rotations and 
     if (UseSkeleton) {
-      SkeletalLines(id, skeleton);
+      SkeletalLines(id, skeleton, has_overlap);
     }
 
     return track;
   }
 
   /// <summary>Render lines between skeletal joints</summary>
-  protected void SkeletalLines(string id, Transform skeleton) {
+  protected void SkeletalLines(string id, Transform skeleton, bool has_overlap) {
     for(int i = 0; i < joint_pair_idx.GetLength(0); i++) {
       string a = joint_names[joint_pair_idx[i,0]];
       string b = joint_names[joint_pair_idx[i,1]];
-      RenderLine(skeleton, GoldenColor(id, v: 0.5f), a, b);
+      Color color = GoldenColor(id, v: 0.5f);
+      if (has_overlap) {
+        color = Color.white;
+      }
+      RenderLine(skeleton, color, a, b);
     }
   }
 
@@ -259,23 +374,15 @@ public class WebsocketZedVisualizer : MonoBehaviour
     if (!joint) {
       GameObject o = GameObject.CreatePrimitive(PrimitiveType.Sphere);
       // If the standard shader is not assigned, color assignment in WebGL fails
-      //Debug.Log(joint_name);
       // /frames information is in millimeters
       // unity uses metric units 1 meter per unit 
       // take the average of a person's key points - center of mass - create a physics collider - unity capsule collider - set radius - can check for collisions
       // mesh shows up but no physical properties - colliders can or cannot intersect
       // create a mesh (cylinder or sphere) creates a covid bubble - 3D alpha property on shader and it's how we render it, color or texture applied, could draw a circle, 2D; 
       // can check interactions between colliders
-      if(joint_name == "LAnkle" ^ joint_name == "RAnkle"){
-        o = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-        o.transform.localScale = new Vector3(2f, 0.00001f, 1f);
-      }
-      else{
-        o.transform.localScale = new Vector3(JointScale, JointScale, JointScale);
-      }
       o.GetComponent<Renderer>().material.shader = Shader.Find("Standard");
       o.name = joint_name;
-      //o.transform.localScale = new Vector3(JointScale, JointScale, JointScale);
+      o.transform.localScale = new Vector3(JointScale, JointScale, JointScale);
       o.transform.parent = skeleton;
       joint = o.transform;
     }
